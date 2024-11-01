df9e871e4a38a7d44b30435b933ce011c71ab793
Josh Borrow <josh@joshborrow.com>
2024-11-01 14:28:10 -0400

    Add catalog and deprecate 'Map'

---
+++ hippometa/catalog.py
@@ -0,0 +1,18 @@
+from typing import Literal
+
+from hippometa.base import BaseMetadata
+
+
+class CatalogMetadata(BaseMetadata):
+    """
+    A catalog (potentially of resolved sources).
+    """
+
+    metadata_type: Literal["catalog"] = "catalog"
+
+    file_type: Literal["csv", "fits", "hdf5"]
+    column_description: dict[str, str]
+
+    telescope: str | None = None
+    instrument: str | None = None
+    release: str | None = None
