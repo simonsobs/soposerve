1474dfa35a4a12b6992dd741179ed348beff82f4
jeremy <69446567+jernestmyers@users.noreply.github.com>
2024-11-14 10:05:19 -0500

    Wire up routes, queries, templates for metadata searching (#19)
    * Reformat number input query params for easier backend parsing
    
    * Add number ranges, regex, and case insensitivity to metadata queries
    
    * Fix metadata form's layout
    
    * Render select elements for literal types
    
    * Provide user more type info in placeholders, titles
    
    * Add querying for list[str] types
    
    * Add some comments to meta search logic
    
    * Improve metadata search form layout
    
    * DRY up some query logic and better guard type checking

--- hipposerve/web/__init__.py
+++ hipposerve/web/__init__.py
@@ -6,7 +6,8 @@ NOTE: Code coverage is an explicit NON-goal for the web
       coverage metrics.
 """
 
-from typing import Annotated, Literal, get_args, get_origin
+import types
+from typing import Annotated, Literal, Union, get_args, get_origin
 
 import httpx
 from beanie import PydanticObjectId
@@ -80,6 +81,32 @@ async def collection_view(request: Request, id: PydanticObjectId):
     )
 
 
+# --- Search ---
+
+
+# A simple utility function to test if a type contains a list
+def has_list(field_type):
+    if get_origin(field_type) is list:
+        return True
+    elif get_origin(field_type) in {types.UnionType, Union}:
+        args = get_args(field_type)
+        return any(get_origin(arg) is list for arg in args)
+    else:
+        return False
+
+
+# Grabs the list argument, but assumes we wouldn't have more than one
+# list in a union type
+def get_list_arg(field_type):
+    args = get_args(field_type)
+    if len(args) == 1:
+        return args[0].__name__
+    else:
+        list_arg = [arg for arg in args if get_origin(arg) is list]
+        return get_args(list_arg[0])[0].__name__
+
+
+# Query and render search results from the navigation bar's "search by name" option
 @web_router.get("/search/results", response_class=HTMLResponse)
 async def search_results_view(
     request: Request, q: str = None, filter: str = "products"
@@ -97,16 +124,59 @@ async def search_results_view(
     )
 
 
+# Query and render search results for the more complex metadata request
 @web_router.get("/searchmetadata/results", response_class=HTMLResponse)
 async def searchmetadata_results_view(
     request: Request, q: str = None, filter: str = "products"
 ):
     query_params = dict(request.query_params)
-    metadata_filters = {
-        key: value
-        for key, value in query_params.items()
-        if value and key != "metadata_type"
-    }
+    # Determine which metadata_class we're searching on so we can get the fields;
+    # we will use the fields with the query_params to craft type-specific queries
+    metadata_class = next(
+        (cls for cls in ALL_METADATA if cls.__name__ == query_params["metadata_type"])
+    )
+    metadata_fields = metadata_class.__annotations__
+
+    # Will hold the queries as we craft them below
+    metadata_filters = {}
+
+    for key, value in query_params.items():
+        if key != "metadata_type":
+            # Literals don't need any regex or case insensitity
+            if getattr(metadata_fields[key], "__origin__", None) is Literal:
+                metadata_filters[key] = value
+            elif has_list(metadata_fields[key]):
+                list_type_arg = get_list_arg(metadata_fields[key])
+                # Add some number-specific query logic to list[int] and list[float]
+                if list_type_arg == "int" or list_type_arg == "float":
+                    numerical_values = value.split(",")
+                    min = (
+                        numerical_values[0]
+                        if numerical_values[0] != "undefined"
+                        else None
+                    )
+                    max = (
+                        numerical_values[1]
+                        if numerical_values[1] != "undefined"
+                        else None
+                    )
+                    if min is not None:
+                        metadata_filters[key] = {"$gte": min}
+                    if max is not None:
+                        metadata_filters[key] = metadata_filters.get(key, {})
+                        metadata_filters[key]["$lte"] = max
+                # Add query logic to list[str] types
+                elif list_type_arg == "str":
+                    metadata_filters[key] = {
+                        "$in": [v.strip() for v in value.split(",")]
+                    }
+                else:
+                    # Apply a default query as a fallback
+                    metadata_filters[key] = {"$regex": value, "$options": "i"}
+            else:
+                # Default queries include regex and case insensitivity
+                metadata_filters[key] = {"$regex": value, "$options": "i"}
+
     results = await product.search_by_metadata(metadata_filters)
 
     return templates.TemplateResponse(
@@ -137,13 +207,23 @@ async def search_metadata_view(request: Request):
             other_fields = {}
 
             for field_key, field_type in fields.items():
+                # Don't render metadata fields for dict type
                 if get_origin(field_type) is not dict:
-                    if getattr(field_type, "__origin__", None) is Literal:
-                        literal_values = field_type.__args__
-                        other_fields[field_key] = " | ".join(literal_values)
-                    elif get_origin(field_type) is list:
-                        list_type = get_args(field_type)[0]
-                        list_type_str = f"list[{list_type.__name__}]"
+                    # Let's format literals to make it easier to render as
+                    # a select element in the template
+                    if (
+                        getattr(field_type, "__origin__", None) is Literal
+                        and field_key != "metadata_type"
+                    ):
+                        literals = ", ".join(map(str, get_args(field_type)))
+                        other_fields[field_key] = f"Literals: {literals}"
+                    # Similarly, let's format list types for the template's
+                    # conditional rendering for string lists and number ranges
+                    # NOTE: Will ignore any other types in a union if has_list
+                    # returns True
+                    elif has_list(field_type):
+                        list_type_arg = get_list_arg(field_type)
+                        list_type_str = f"list[{list_type_arg}]"
 
                         if list_type_str in ["list[int]", "list[float]"]:
                             number_fields[field_key] = list_type_str
