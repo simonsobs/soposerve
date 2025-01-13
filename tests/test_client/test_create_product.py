83f9208b6bbfa085496b06ea3c41baeacb9c9ff5
Josh Borrow <josh@joshborrow.com>
2025-01-13 13:51:05 -0500

    Add tests using live server

---
+++ tests/test_client/test_create_product.py
@@ -0,0 +1,57 @@
+"""
+Tests the create and upload product flow, to test e.g. multipart uploads.
+"""
+
+import random
+
+from hippoclient import product
+from hippoclient.core import Client
+from hippometa import SimpleMetadata
+
+
+def test_upload_no_multipart(server, tmp_path):
+    client = Client(
+        api_key=server["test_user_api_key"],
+        host=server["url"],
+        verbose=True,
+        use_multipart_size=0,
+    )
+
+    with open(tmp_path / "test.bin", "wb") as f:
+        for _ in range(64):  # 64MB
+            f.write(random.randbytes(1024 * 1024))
+
+    id = product.create(
+        client=client,
+        name="test_product",
+        description="test_description",
+        metadata=SimpleMetadata(),
+        sources=[tmp_path / "test.bin"],
+        source_descriptions=[None],
+    )
+
+    product.delete(client, id)
+
+
+def test_upload_with_multipart(server, tmp_path):
+    client = Client(
+        api_key=server["test_user_api_key"],
+        host=server["url"],
+        verbose=True,
+        use_multipart_size=32 * 1024,
+    )
+
+    with open(tmp_path / "test.bin", "wb") as f:
+        for _ in range(64):  # 64MB
+            f.write(random.randbytes(1024 * 1024))
+
+    id = product.create(
+        client=client,
+        name="test_product",
+        description="test_description",
+        metadata=SimpleMetadata(),
+        sources=[tmp_path / "test.bin"],
+        source_descriptions=[None],
+    )
+
+    product.delete(client, id)
